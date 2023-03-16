a733b9beea20e30ff810883c845c1c83c9e3208e
ULTRONPL <98317764+ULTRONPL@users.noreply.github.com>
2023-03-16 20:03:59 +0100

    Update vod.py

--- vod.py
+++ vod.py
@@ -1,17 +1,14 @@
 import requests
 import re
 while True:
+    #fake id
     link_z = input()
     link_bez = link_z.replace("\\", "")
-    print(link_bez, "<--- link bez (\)")
     id_z = link_bez.split("/")[-1]
-    print(id_z)
     falszywe_id = id_z.split(",")[-1]
-    print(falszywe_id, "<--- nieprawdziwe id video")
+    #real id
     link_api_id = "https://vod.tvp.pl/api/products/vods/" + str(falszywe_id) + "?lang=pl&platform=BROWSER"
-    print(link_api_id)
     r = requests.get(link_api_id)
-    print(r, "<--- jeśli wartość 200 to poprawnie udało uzyskać strone")
     strona_z_id = r.content
     file = open("data\\id.txt", "w")
     file.write(str(strona_z_id))
@@ -19,25 +16,24 @@ while True:
     with open("data\\id.txt", "r") as id_file:
         first_line = id_file.readline()
         id_video_z = first_line[35:65]
-        print(id_video_z)
+        #print(id_video_z)
         id_video = re.sub("[^0-9^.]", "", id_video_z)
-        print(id_video)
+        #print(id_video)
         file = open("data\\id.txt", "w")
         file.write(str(id_video))
         file.close()
+    #api
     link_video_strona = "https://vod.tvp.pl/sess/TVPlayer2/api.php?id=" + str(id_video) + "&@method=getTvpConfig&@callback=callback"
-    print(link_video_strona)
-    videosite = requests.get(link_video_strona)
-    strona_video_plik = videosite.content
-    video_strona_bez = str(strona_video_plik).replace("\\", "")
-    print(video_strona_bez)
-    file = open("data\\video_strona.txt", "w")
-    file.write(str(video_strona_bez))
-    file.close()    
-    with open("data\\video_strona.txt") as file:    
-        for line in file:
-            pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-            urls = re.findall(pattern, line)
-            urls_all = print(urls)
-    url_bez_enter = str(urls).replace(',', '\n')
-    print(url_bez_enter, "\n                                                                                                            pliki tylko .mp4 reszta nieważna \n                                                                                                            video-(większy numer=lepsza jakość).mp4")
+    headers = {"Accept": "application/json"}
+    videosite = requests.get(link_video_strona, headers=headers)
+    json_data = videosite.json()
+    replace = str(json_data).replace(",", "\n")
+    final = str(replace).replace("'", "")
+    mp4_links = re.findall("https?:\/\/\S+\.mp4", final)
+    for link in mp4_links:
+        print(link)
+    print("video-(większy numer=lepsza jakość).mp4")
+    
+
+    while True:
+        pass
